364af8fa64500f625359c767aa4222bc30da680c
javlonsadullayev <javlonsadullayev1906@gmail.com>
2025-01-24 14:13:27 +0300

    .

--- formats.py
+++ formats.py
@@ -1,13 +1,31 @@
 import json
 
-
-f = open('users.json')
+f  = open('people.json')
 
 json_data = f.read()
-users_list = json.loads(json_data)
 
-
-males = list(filter(lambda user: user['gender'] == 'Male', users_list))
-females = list(filter(lambda user: user['gender'] == 'Female', users_list))
-
-print(len(males), len(females))
+user_list = json.loads(json_data)
+count_male = 0
+count_female = 0
+for user in user_list:
+    if user["gender"] == "Male":
+         count_male +=1
+    if user["gender"] == "Female":
+        count_female +=1
+print("Erkakalar soni:",count_male,"ta")
+print("Ayollar soni: ",count_female,"ta")
+print("\n")
+print("Hindistonlik foydalanuvchilar")
+for user in user_list:
+    if user["country"] == "India":
+        print(user)
+print("\n")
+print("20 yoshdan kichik foydalanuvchilar")
+for user in user_list:
+    if user["age"] < 20:
+        print(user)
+print("\n")
+print("Muhandislar")
+for user in user_list:
+    if user["job"] == "Engineer":
+        print(user)
